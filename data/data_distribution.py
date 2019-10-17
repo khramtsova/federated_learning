1b04ff933746f13fc384f4b7caa3e656259d2a67
khramtsova <2katenka@mail.ru>
2019-10-17 18:07:18 +0200

    Specify a parameter sub_labels - a subgroup of labels, among which the distribution of a certain type(iid, non_iid or non_iid_exclusive) will be performed.
    Important in case when the test and train labels are different

--- data/data_distribution.py
+++ data/data_distribution.py
@@ -1,6 +1,7 @@
 import itertools
 import random
 from collections import Counter
+from functools import wraps
 
 
 class Distribute:
@@ -8,42 +9,64 @@ class Distribute:
     def __init__(self,  num_agents, num_classes):
         self.n_class = num_classes
         self.n_agent = num_agents
+        self.distr = [[0 for j in range(self.n_class)] for i in range(self.n_agent)]
 
-    def create_iid(self):
+    def _create_distribution(func):
+        @wraps(func)
+        def wrap(arg, n_labels_per_agent=None, sub_labels=None):
+            # if there are some labels in the argument
+            if sub_labels:
+                arg._change_labels(sub_labels)
+            else:
+                arg._set_default_labels()
+            func(arg, n_labels_per_agent)
+            return arg.distr
+        return wrap
+
+    @_create_distribution
+    def create_iid(self, _):
         val = 1/self.n_agent
-        distr = [[val for j in range(self.n_class)] for i in range(self.n_agent)]
-        return distr
+        for j in self.labels:
+            for i in range(self.n_agent):
+                self.distr[i][j] = val
+        #self.distr = [[val for j in self.labels] for i in range(self.n_agent)]
+        #return distr
 
     # Each client have a certain number of labels (2 for original paper of FedAvr)
     # The labels may cross
+    @_create_distribution
     def create_non_iid(self, n_labels):
+        print(self.labels)
 
-        agents = [random.sample(range(self.n_class), n_labels)for j in range(self.n_agent)]
+        agents = [random.sample(self.labels, n_labels)for j in range(self.n_agent)]
 
         count = Counter(i for i in list(itertools.chain.from_iterable(agents)))
-        print(count)
-        distr = [[0 for j in range(self.n_class)] for i in range(self.n_agent)]
+        #print(count)
 
-        for i,ag in enumerate(agents):
+        for i, ag in enumerate(agents):
             for j in ag:
-                distr[i][j] = 1/count[j]
-        return distr
+                self.distr[i][j] = 1/count[j]
 
     # Each client have a certain number of labels (2 for original paper of FedAvr)
     # The labels are unique for each client
+    @_create_distribution
     def create_non_iid_exclusive(self, n_labels):
-
-        assert self.n_agent * n_labels <= self.n_class
-        random_id = random.sample(range(self.n_class), self.n_agent * n_labels)
-        distr = [[0 for j in range(self.n_class)] for i in range(self.n_agent)]
+        print("n_labels",n_labels)
+        assert self.n_agent * n_labels <= len(self.labels)
+        random_id = random.sample(self.labels, self.n_agent * n_labels)
 
         for i in range(self.n_agent):
             for j in range(n_labels):
-                distr[i][random_id.pop(0)] = 1
-        return distr
+                self.distr[i][random_id.pop(0)] = 1
+
+    def _change_labels(self, new_labels):
+        self.labels = new_labels
+
+    def _set_default_labels(self):
+        self.labels = [i for i in range(self.n_class)]
 
 
 if __name__ == "__main__":
 
-    distribution = Distribute(2, 10)#.create_iid()
-    print(distribution.create_non_iid_exclusive(5))
+    distribution = Distribute(2, 10)
+    distribution.create_iid(sub_labels=[1, 2, 3, 4, 5])
